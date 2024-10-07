be0bda51820c4d278742a731a7aeda3f6c284070
anishregmi17 <anishregminaglibang@gmail.com>
2024-10-07 23:54:53 +0545

    IMP:route link for contact from service page

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -16,15 +16,23 @@ onMounted(async () => {
 
 <template>
   <div class="services-page py-10">
-    <h1 class="text-4xl font-bold text-center mb-8">My Services</h1>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4">
+    <h1 class="text-4xl font-bold text-center mb-10">My Services</h1>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-10 px-6 lg:px-32">
       <div
         v-for="(service, index) in services"
         :key="index"
-        class="bg-white rounded-lg shadow-lg p-6"
+        class="bg-white border border-gray-300 rounded-lg shadow-lg p-8 hover:shadow-xl transition-shadow duration-300"
       >
-        <h2 class="text-xl font-semibold mb-4">{{ service.title }}</h2>
-        <p class="text-gray-600">{{ service.description }}</p>
+        <h2 class="text-2xl font-semibold mb-4 text-black">
+          {{ service.title }}
+        </h2>
+        <p class="text-gray-600 mb-6">{{ service.description }}</p>
+        <button
+          class="bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600"
+        >
+          <router-link to="/contacts">contact for more</router-link>
+        </button>
       </div>
     </div>
   </div>
