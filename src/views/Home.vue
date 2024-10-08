38f08b920f34fbe5d59771f1b5f4e6a437300c52
anishregmi17 <anishregminaglibang@gmail.com>
2024-10-08 22:02:29 +0545

    ADD:home page and profile image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
-<script setup>
-import { ref } from "vue";
-const title = ref("Hello !!!");
-</script>
+<script setup></script>
 
 <template>
-  <div class="home">
-    <h1>{{ name }}</h1>
-    <p>I'm Anish Regmi (Bsc. CSIT Student)</p>
-    <router-link to="/projects">Check out my projects</router-link>
+  <div
+    class="min-h-screen bg-gray-100 flex flex-col justify-center items-center relative"
+  >
+    <div class="flex flex-col items-center space-y-4">
+      <img
+        src="../assets/anish2.jpg"
+        alt="Profile"
+        class="rounded-full w-56 h-56 object-cover shadow-2xl transition-transform duration-300 hover:scale-105"
+      />
+
+      <div id="about" class="text-center">
+        <h2 class="text-3xl font-semibold text-gray-800">
+          Hi, I'm Anish Regmi
+        </h2>
+        <p class="mt-2 text-gray-600 text-lg">CS Student and Tech Enthusiast</p>
+      </div>
+
+      <a
+        href="#projects"
+        class="mt-4 px-6 py-3 bg-blue-500 text-white rounded-lg text-lg font-medium hover:bg-blue-600 transition duration-300 shadow-md"
+      >
+        Check out my Projects
+      </a>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.home {
-  text-align: center;
+.min-h-screen {
+  min-height: calc(100vh - 4rem - 4rem);
 }
 </style>
